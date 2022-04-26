c4e477ea20364a08ec4227bea82abb82d7e4b68f
papasmf1 <papasmf1@gmail.com>
2022-04-26 14:01:13 +0900

    정규표현식 연습

--- demoStr.py
+++ demoStr.py
@@ -8,5 +8,19 @@ print(strA.capitalize())
 print(strA.count("p"))
 print(strA.count("p", 7))
 print("MBC2580".isalnum())
-print("MBC:2580".isalnum())
+print( "MBC:2580".isalnum()) 
+print("2580".isdecimal())
+strData = "<<< spam and ham >>>"
+result = strData.strip("<> ")
+print(strData)
+print(result)
+
+print(result)
+result2 = result.replace("spam", "spam egg")
+print(result2)
+lst = result2.split()
+print(lst)
+print(" ".join(lst))
+
+
 
